4f3fe0302f1301544631c1356ba71e9bc0f6ff64
Paulo Cesar <paulo.cardoso2408@gmail.com>
2026-03-31 10:14:26 -0300

    master: adicionado documentação de API

--- src/form/form.dto.ts
+++ src/form/form.dto.ts
@@ -1,55 +1,72 @@
 import { IsNumber, IsOptional, IsString, IsEmail } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateFormDto {
     @IsOptional() // Se você quer que o id seja opcional no create
     @IsNumber()
     id?: number;
 
+    @ApiProperty({ description: 'Nome do consultor associado SGA' })
     @IsString()
     consultor_associado_sga: string;
 
+    @ApiProperty({ description: 'Nome da cooperativa do consultor' })
     @IsString()
     nome_cooperativa_consultor: string;
 
+    @ApiProperty({ description: 'Email do voluntário SGA' })
     @IsEmail()
     email_voluntario_sga: string;
 
+    @ApiProperty({ description: 'Tipo do consultor' })
     @IsString()
     type_consultant: string;
 
+    @ApiProperty({ description: 'Nome do associado SGA' })
     @IsString()
     nome_associado_sga: string;
 
+    @ApiProperty({ description: 'Telefone do associado SGA' })
     @IsString()
     telefone_associado_sga: string;
 
+    @ApiProperty({ description: 'Placa do associado SGA' })
     @IsString()
     plate_associate: string;
 
+    @ApiProperty({ description: 'Modelo do veículo associado SGA' })
     @IsString()
     modelo_associado_sga: string;
 
+    @ApiProperty({ description: 'Tipo de consulta: externo e interno' })
     @IsString()
     cliente_type_consultant: string;
 
+    @ApiProperty({ description: 'Nome do terceiro' })
     @IsString()
     nome_terceiro: string;
 
+    @ApiProperty({ description: 'Telefone do terceiro' })
     @IsString()
     telefone_terceiro: string;
 
+    @ApiProperty({ description: 'Placa do terceiro' })
     @IsString()
     placa_terceiro: string;
 
+    @ApiProperty({ description: 'Descrição da demanda' })
     @IsString()
     description_associate: string;
 
+    @ApiProperty({ description: 'Departamento' })
     @IsString()
     department: string;
 
+    @ApiProperty({ description: 'Pesquisa Evogard Edu' })
     @IsString()
     pes_evogard_edu: string;
 
+    @ApiProperty({ description: 'Pesquisa Cliente Resolveu' })
     @IsString()
     pes_cliente_resolveu: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
 
+  const config = new DocumentBuilder()
+    .setTitle('API Auto Analytics')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
+
 bootstrap();
