3bd1f6b845efe29b7c370cc33634e12c8c76c728
Paulo Cesar <paulo.cardoso2408@gmail.com>
2026-04-02 12:24:03 -0300

    master: adicionado ajuste no main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
   });
+
 
   app.setGlobalPrefix('api');
 
